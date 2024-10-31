f6d3310316cf35f9bfa53ea87683b05a403598ce
Rajan <steamkeliyeid@gmail.com>
2024-10-31 19:07:22 +0530

    lil to no progress

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,13 @@
+// backend\routes\auth.routes.js
+import express, { Router } from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router;
