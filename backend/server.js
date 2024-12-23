2128e90ecd042293248cda22044c02fd1f83fd1b
Rajan <steamkeliyeid@gmail.com>
2024-12-24 02:49:18 +0530

    signup,login pages ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -14,7 +15,12 @@ dotenv.config();
 //adding another middle layer
 app.use(express.json()); //to parse the incoming request from the json payloads (from req.body)
 app.use(cookieParser());
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use("/api/auth", authRoutes); //middle layer    
 app.use("/api/messages", messageRoutes); //middle layer    
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage.jsx'
+import Settingspage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import StatsPage from './pages/StatsPage.jsx'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore.js'
+
+import { Loader } from 'lucide-react'
+import { Toaster } from "react-hot-toast"
+ 
+  
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+  
+  if (isCheckingAuth && !authUser) 
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+    
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/> }/>
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage/>: <Navigate to="/" />}/>
+        <Route path="/settings" element={<Settingspage/>}/>
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+        <Route path="/stats" element={authUser ? <StatsPage /> : <Navigate to="/login"/>} />
+      </Routes>
+
+      <Toaster />
+    </div>
   )
 }
 
